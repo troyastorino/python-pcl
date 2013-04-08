b731d8ff11217b81e138857f084aa1720ec756dd
Troy Astorino <troyastorino@gmail.com>
2013-04-08 13:40:51 -0400

    Edited setup.py to have Homebrew include dirs

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import numpy as np
 
 setup(name='python-pcl',
       description='pcl wrapper',
@@ -12,7 +13,7 @@ setup(name='python-pcl',
       ext_modules=[Extension(
                    "pcl",
                    ["pcl.pyx", "minipcl.cpp"],
-                    include_dirs=["/usr/include/pcl-1.5", "/usr/include/eigen3/"],
+                    include_dirs=[np.get_include(), "/usr/local/include/pcl-1.7", "/usr/local/include/eigen3/"],
                     libraries=["pcl_segmentation", "pcl_io", "OpenNI",
                                "usb-1.0", "pcl_filters", "pcl_sample_consensus",
                                "pcl_features", "pcl_surface", "pcl_search", "pcl_kdtree", "pcl_octree",
